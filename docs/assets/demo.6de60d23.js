be731563af4458a7df2c934b92a0e79f4bd3d8b3
zhangmengxue.jane <zhangmengxue.jane@sh-zhangmx>
2022-06-15 16:16:35 +0800

    add

---
+++ docs/assets/demo.6de60d23.js
@@ -0,0 +1,1 @@
+import{d as e,p as t,h as r}from"./vendor.36c2bc47.js";var a=e({setup(){return()=>t("div",null,[r("hello tsx")])}});export{a as default};
---
+++ docs/assets/testPinia.dafb9210.js
@@ -0,0 +1,1 @@
+import{d as e}from"./vendor.36c2bc47.js";const p=e({setup(t){return(n,r)=>null}});export{p as default};
