9b4f76063bc3b9691babbd560bda2dd99ab0411f
zhangmengxue.jane <zhangmengxue.jane@sh-zhangmx>
2022-06-15 22:03:26 +0800

    test

---
+++ docs/assets/demo.db198e42.js
@@ -0,0 +1,1 @@
+import{d as e,p as t,h as r}from"./vendor.7b2428d5.js";var a=e({setup(){return()=>t("div",null,[r("hello tsx")])}});export{a as default};
---
+++ docs/assets/testPinia.29d54710.js
@@ -0,0 +1,1 @@
+import{d as e}from"./vendor.7b2428d5.js";const p=e({setup(t){return(n,r)=>null}});export{p as default};
