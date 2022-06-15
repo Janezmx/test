5a86761249e3f2775f7be4a3179824eda0ba5661
zhangmengxue.jane <zhangmengxue.jane@sh-zhangmx>
2022-06-15 19:36:54 +0800

    test

---
+++ docs/assets/demo.25415433.js
@@ -0,0 +1,1 @@
+import{d as e,p as t,h as r}from"./vendor.d7d0cee6.js";var a=e({setup(){return()=>t("div",null,[r("hello tsx")])}});export{a as default};
---
+++ docs/assets/testCssModel.82455169.js
@@ -0,0 +1,1 @@
+import{d as e,p as o,h as l}from"./vendor.d7d0cee6.js";const a="_moduleClass_15i7z_1";var s={moduleClass:a},d=e({setup(){return console.log("css--model",s),()=>o("div",{class:`root  ${s.moduleClass}`},[l("\u6D4B\u8BD5css-modules")])}});export{d as default};
