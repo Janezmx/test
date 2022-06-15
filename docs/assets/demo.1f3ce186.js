4ab8520ebe29ff60a78576f0507e20ad7fbce600
zhangmengxue.jane <zhangmengxue.jane@sh-zhangmx>
2022-06-15 22:28:29 +0800

    test

---
+++ docs/assets/demo.1f3ce186.js
@@ -0,0 +1,1 @@
+import{d as e,v as t,h as r}from"./vendor.4e393f88.js";var a=e({setup(){return()=>t("div",null,[r("hello tsx")])}});export{a as default};
---
+++ docs/assets/testPinia.a6dda991.js
@@ -0,0 +1,1 @@
+import{d as e}from"./vendor.4e393f88.js";const p=e({setup(t){return(n,r)=>null}});export{p as default};
