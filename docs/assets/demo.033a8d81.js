0ddc1427a7e1dc4f469194e3f384fff5a5dc6897
zhangmengxue.jane <zhangmengxue.jane@sh-zhangmx>
2022-06-20 01:08:47 +0800

    test

---
+++ docs/assets/demo.033a8d81.js
@@ -0,0 +1,1 @@
+import{d as e,y as t,h as r}from"./vendor.e0f8e8d7.js";var a=e({setup(){return()=>t("div",null,[r("hello tsx")])}});export{a as default};
---
+++ docs/assets/testPinia.427a9d27.js
@@ -0,0 +1,1 @@
+import{d as e}from"./vendor.e0f8e8d7.js";const p=e({setup(t){return(n,r)=>null}});export{p as default};
