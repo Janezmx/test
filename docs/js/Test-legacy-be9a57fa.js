463708290526d90b962094a0bf3c0c25fe523607
zhangmengxue.jane <zhangmengxue.jane@sh-zhangmx>
2022-07-25 18:41:11 +0800

    test

---
+++ docs/js/Test-legacy-be9a57fa.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-db8a9bc6.js"],(function(e){"use strict";var t;return{setters:[function(e){t=e._}],execute:function(){e("T",t({name:"Test"},[["render",function(e,t,n,r,s,c){return" test "}]]))}}}));
