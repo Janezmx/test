5729a778e6c73453813dd89a7163a23546305eda
zhangmengxue.jane <zhangmengxue.jane@sh-zhangmx>
2022-07-25 18:25:02 +0800

    test

---
+++ docs/js/Test-legacy-e7b997a4.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-c801b3c1.js"],(function(e){"use strict";var t;return{setters:[function(e){t=e._}],execute:function(){e("T",t({name:"Test"},[["render",function(e,t,n,r,c,s){return" test "}]]))}}}));
